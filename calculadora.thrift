a982587f553400b5533d84b745ec1a93759fd521
JoseCarlos <jcarloslopez@correo.ugr.es>
2023-04-12 21:25:59 +0200

    (src) Change plantilla thrift para pasarle dimensión de la matriz

--- calculadora.thrift
+++ calculadora.thrift
@@ -1,3 +1,3 @@
 service Calculadora{
-   list<list<double>> suma_matrices(1: list<list<double>> m1, 2:list<list<double>> m2)
+   list<double> suma_matrices(1: list<double> m1, 2: list<i64> dim1, 3:list<double> m2, 4:list<i64> dim2)
 }
